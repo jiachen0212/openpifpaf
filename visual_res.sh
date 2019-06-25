a5f67076d660d4cfa33271f5ed6e762c14580ad5
jiachen0212 <jiachen0212@163.com>
2019-06-25 16:29:24 +0800

    Update visual_res.sh

--- visual_res.sh
+++ visual_res.sh
@@ -2,5 +2,5 @@ export PATH="/home..../envs/py36torch0.4/bin:$PATH"
 export PYTHONPATH="/home/...../envs/py36torch0.4/lib/python3.6/site-packages/:$PYTHONPATH"
 
 
-python gt_vision.py
-srun python openpifpaf/show_eval.py  --checkpoint outputs/resnet50block5-pif-paf-edge401-190621-093216.pkl.epoch060
+python gt_vision.py    # draw gt skeleston
+srun python openpifpaf/show_eval.py  --checkpoint outputs/resnet50block5-pif-paf-edge401-190621-093216.pkl.epoch060 # draw pred skeleston
